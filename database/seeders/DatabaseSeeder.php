07ae4189c059a8dd9fe28356401638cbc04d86aa
Dmitriy <nde9@yandex.ru>
2024-02-05 16:24:54 +0300

    gitlab

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,12 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
-        // ]);
+                // ]);
+        $this->call([
+        UserSeeder::class,
+
+    ]);
+
+
     }
 }
