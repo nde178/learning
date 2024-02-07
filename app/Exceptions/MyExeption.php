4232137dcd3c6c469535885e706ee05de1d83f63
Dmitriy <nde9@yandex.ru>
2024-02-07 16:39:50 +0300

    Добавил

---
+++ app/Exceptions/MyExeption.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class MyExeption extends Exception
+{
+    public function context(){
+        return ['data'=>'Данные'];
+    }
+    public function render(){
+        
+    }
+
+
+}
